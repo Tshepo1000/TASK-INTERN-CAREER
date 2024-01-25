1dd76e81c8c09b5894d351d663fac139f414e9f4
Tshepo1000 <tshepotubatsi@gmail.com>
2024-01-26 01:25:14 +0200

    Login page almost complete

---
+++ School_portal/src/school_portal/School_portal.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package school_portal;
+
+/**
+ *
+ * @author Tshepo T Sekete
+ */
+public class School_portal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ School_portal/src/school_portal/db.java
@@ -0,0 +1,49 @@
+package school_portal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class db 
+{
+    //database connection
+    public static Connection myCon()
+    {
+        // JDBC URL, username, and password of MySQL server
+        String url = "jdbc:mysql://localhost:3306/school_system?useSSL=false";
+        String user = "Tshepo";
+        String password = "&RWjJH/8A6q$B/B";
+
+        Connection connection = null;
+        try
+        {
+            // Register the JDBC driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Open a connection
+            connection = DriverManager.getConnection(url, user, password);
+        }
+        catch(ClassNotFoundException | SQLException e) 
+        {
+            e.printStackTrace();
+
+        }
+        finally
+        {
+            try
+            {
+                // close connection
+                if(connection == null)
+                {
+                    connection.close();
+                }
+            }
+            catch(SQLException sqe)
+            {
+                JOptionPane.showMessageDialog(null, "Something went wrong");
+            }
+        }
+        return connection;
+    }
+}
